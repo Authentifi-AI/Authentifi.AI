1c1f1bf5929107a51edf6517f89336ec9c4d8119
AuthentifiAI-Udi <udi@authentifi.ai>
2025-02-19 15:19:07 -0500

    Added support for footer section being scalable

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function App() {
             <Navbar />
             <Hero />
             <Feature />
-            {/* <Footer /> */}
+            <Footer />
         </>
     )
 }
