36fd429c31d00aa218b5a774b60c83a6144b0dfd
AuthentifiAI-Udi <udi@authentifi.ai>
2025-03-09 23:17:27 -0400

    Updated to a grid layout

--- src/HomePage/Hero.js
+++ src/HomePage/Hero.js
@@ -1,7 +1,6 @@
 import './Hero.css'
 import { motion } from "motion/react"
 import { useMediaQuery } from 'react-responsive'
-import { useNavigate } from 'react-router-dom';
 
 
 export default function Hero() {
@@ -14,13 +13,13 @@ export default function Hero() {
         RightsectionVariants = {
             initial: { opacity: 0, x: -75 },
             whileInView: { opacity: 1, x: 0, transition: { delay: 0.2, duration: 0.4 } },
-            viewport: { once: false, amount: 0.1 }
+            viewport: { once: true, amount: 0.1 }
         };
 
         LeftsectionVariants = {
             initial: { opacity: 0, x: 75 },
             whileInView: { opacity: 1, x: 0, transition: { delay: 0.2, duration: 0.4 } },
-            viewport: { once: false, amount: 0.7 }
+            viewport: { once: true, amount: 0.7 }
         }
     }
 
@@ -29,40 +28,38 @@ export default function Hero() {
         LeftsectionVariants = {
             initial: { opacity: 0, x: 75 },
             whileInView: { opacity: 1, x: 0, transition: { delay: 0.1, duration: 0.25 } },
-            viewport: { once: false }
+            viewport: { once: true }
 
         }
     }
 
     return (
         <>
-            <section class="inline-flex h-screen w-screen justify-center items-center">
+            <section class="grid justify-center text-center lg:grid-cols-2 lg:h-screen">
                 <motion.div
                     initial={RightsectionVariants.initial}
                     whileInView={RightsectionVariants.whileInView}
                     viewport={RightsectionVariants.viewport}
-                    className='flex-col pr-30 -mt-30'>
-                    <h1 className='text-8xl text-white font-light'>Authentifi AI</h1>
-                    <div className='inline-flex'>
-                        <img src="logo.png" alt="Company Logo" className='h-auto w-30 -mt-4 -mr-3' />
-                        <p class='text-3xl text-gray-300 align-baseline text-left'>AI-powered authorship <br></br>verification</p>
+                    className='sm:max-lg:w-full sm:max-lg:h-screen content-center -mt-40'>
+                    <h1 className='font-medium sm:max-lg:text-9xl lg:text-7xl 2xl:text-9xl text-white mb-10'>Authentifi AI</h1>
+                    <div className='inline-flex sm:max-lg:mb-30'>
+                        <img src="logo.png" alt="Company Logo" className='h-auto sm:max-lg:w-30 lg:w-40 sm:max-lg:mt-4 lg:-mt-8 sm:max-lg:mr-3 lg:-mr-8' />
+                        <p class='sm:max-lg:text-6xl lg:text-3xl text-gray-300 align-baseline text-left'>AI-powered authorship <br></br>verification</p>
                     </div>
-                    <div className='w-fit flex-col text-center justify-center '>
-                    <p class="text-2xl text-gray-100">Stay in control of your work while using AI</p>
-                    
-                        <button className='bg-blue-400 rounded-xl text-lg text-white font-bold pt-2 pb-2 pr-4 pl-4 shadow-xl border-1 border-gray-600 hover:bg-green-700 hover:cursor-pointer mt-5 mb-5 '>Register</button>
-                    
-                    <p class="text-xl text-yellow-300">Be among the first to experience it!</p>
-                    </div>
+                    <p class="sm:max-lg:text-4xl lg:text-3xl text-gray-100">Stay in control of your work while using AI</p>
+
+                    <button className='bg-blue-400 rounded-xl sm:max-lg:text-4xl lg:text-2xl text-white font-bold pt-2 pb-2 pr-4 pl-4 shadow-xl border-1 border-gray-600 hover:bg-green-700 hover:cursor-pointer mt-5 mb-5 '>Register</button>
+
+                    <p class="sm:max-lg:text-5xl lg:text-3xl text-yellow-300">Be among the first to experience it!</p>
                 </motion.div>
 
                 <motion.div
                     initial={LeftsectionVariants.initial}
                     whileInView={LeftsectionVariants.whileInView}
                     viewport={LeftsectionVariants.viewport}
-                    class="flex-col">
-                    <h2 className='text-gray-200 text-3xl'>Meet our student lucy...</h2>
-                    <video controls className='w-auto h-80 rounded-xl'>
+                    class="sm:max-lg:h-screen text-left sm:max-lg:p-10 lg:p-5 lg:content-center">
+                    <h2 className='text-gray-200 sm:max-lg:text-4xl lg:text-3xl sm:max-lg:mb-20'>Meet our student lucy...</h2>
+                    <video controls className='w-auto h-auto rounded-xl bg-white'>
                         <source src="HeroLandingPageVideo.mp4" type="video/mp4" />
                     </video>
                 </motion.div>
--- src/Public/Navbar.js
+++ src/Public/Navbar.js
@@ -1,21 +1,19 @@
-export default function Navbar()
-{
+export default function Navbar() {
 
-  function Item({value, Link})
-{
+  function Item({ value, Link }) {
+    return (
+      <a href={Link} className="sm:max-lg:text-2xl text-white p-5 -mr-2">{value} </a>
+    )
+  }
   return (
-    <a href={Link}className="text-white p-5 -mr-2">{value} </a>
-  )
-}
-    return(
-        <>
-        <nav className="navbar bg-blue-950 flex justify-baseline align-middle pl-1 w-full top-0 z-50 min-h-2">
+    <>
+      <nav className="navbar bg-blue-950 flex justify-baseline align-middle pl-1 w-full top-0 z-50 min-h-2">
         <img src='logo.png' alt='Logo png' className='w-20 h-auto -mb-4 -ml-3 -mr-8'></img>
         <Item value={"AuthentifiAI"} Link={"/"}></Item>
         <Item value={"Login"} Link={"Login"}></Item>
         <Item value={"Register"} Link={"Register"}></Item>
       </nav>
-        </>
-    )
+    </>
+  )
 }
 
