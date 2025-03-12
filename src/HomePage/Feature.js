fa8beb12730fb7909b4136b706aba3b5cae72fcd
AuthentifiAI-Udi <udi@authentifi.ai>
2025-03-12 15:54:23 -0400

    Added full support for landing page with tailwindcss

--- src/HomePage/Feature.js
+++ src/HomePage/Feature.js
@@ -1,19 +1,8 @@
-// import "./Feature.css"
 import { motion } from "motion/react"
 import { useMediaQuery } from 'react-responsive'
 
 
 function Card({ img_src, img_alt, text_value }) {
-    return (
-        <div className="bg-white border-1 rounded-2xl text-left w-7/8 lg:w-15/16 mb-20 p-10 pr-7 lg:p-5 2xl:pb-5">
-            <img src={img_src} alt={img_alt} className="w-auto h-50 lg:h-25" />
-            <h2 className='text-black font-bold text-4xl lg:text-xl 2xl:text-2xl mt-10 2xl:mt-20'>{text_value} </h2>
-        </div>
-    );
-}
-
-
-export default function Feature() {
 
     var sectionVariants = {};
     // check the screen size and set variables accordingly
@@ -21,38 +10,55 @@ export default function Feature() {
         sectionVariants = {
             initial: { opacity: 0, x: -50 },
             whileInView: { opacity: 1, x: 0, transition: { delay: 0.2, duration: 0.4 } },
-            viewport: { once: false, amount: 0.7 }
+            viewport: { once: true, amount: 0.7 }
         };
     }
 
     else {
 
         sectionVariants = {
-            initial: { opacity: 0, x: -50 },
+            initial: { opacity: 0.33, x: -50 },
             whileInView: { opacity: 1, x: 0, transition: { delay: 0.2, duration: 0.4 } },
-            viewport: { once: false, amount: 0.2 }
+            viewport: { once: true, amount: 0.2 }
         };
     }
+    return (
+        <motion.div
+        initial={sectionVariants.initial}
+        whileInView={sectionVariants.whileInView}
+        viewport={sectionVariants.viewport}
+         className="bg-white border-1 rounded-2xl text-left pt-2 pl-5 pr-5 pb-5 mb-5">
+            <img src={img_src} alt={img_alt} className="w-auto h-20" />
+            <h2 className='text-black font-bold text-lg mt-5'>{text_value} </h2>
+        </motion.div>
+    );
+}
+
+
+export default function Feature() {
+
+
 
     return (
         <>
-            <motion.section
-                initial={sectionVariants.initial}
-                whileInView={sectionVariants.whileInView}
-                viewport={sectionVariants.viewport}
+            <section
 
-                className="grid lg:grid-cols-3 justify-items-center mb-100 sm:max-lg:-mt-96 lg:pr-10 lg:pl-10 lg:mt-25">
+                className="grid pr-5 pl-5 pb-30">
 
                 <Card img_src="./work.gif" img_alt="Work gif" text_value="Choose your favorite AI platform and begin working"></Card>
                 <Card img_src="./folder.gif" img_alt="Folder gif" text_value="Record your progress in real time, tracking progress and sources"></Card>
                 <Card img_src="./certificate.gif" img_alt="Certificate gif" text_value="Publish your work with confidence"></Card>
 
 
-                <p className="text-3xl text-center font-bold text-gray-100 lg:col-span-3">
+                <motion.p 
+                initial={{opacity: 0.33}}
+                whileInView={{opacity: 1, transition: { delay: 0.1, duration: 0.2 } }}
+                viewport={{ once: true, amount: 0.2 }}
+                className="text-lg text-center font-bold text-gray-100">
                     "Verify your authorship while working with AI to prove your contributions and authenticity."
-                </p>
+                </motion.p>
 
-            </motion.section>
+            </section>
 
 
         </>
--- src/HomePage/Hero.js
+++ src/HomePage/Hero.js
@@ -1,6 +1,7 @@
-import './Hero.css'
-import { motion } from "motion/react"
+import { motion} from "motion/react"
+import { div } from 'motion/react-client';
 import { useMediaQuery } from 'react-responsive'
+
 
 
 export default function Hero() {
@@ -15,51 +16,52 @@ export default function Hero() {
             whileInView: { opacity: 1, x: 0, transition: { delay: 0.2, duration: 0.4 } },
             viewport: { once: false, amount: 0.1 }
         };
-
-        LeftsectionVariants = {
-            initial: { opacity: 0, x: 75 },
-            whileInView: { opacity: 1, x: 0, transition: { delay: 0.2, duration: 0.4 } },
-            viewport: { once: false, amount: 0.7 }
-        }
     }
 
-    else {
-        ;
         LeftsectionVariants = {
-            initial: { opacity: 0, x: 75 },
-            whileInView: { opacity: 1, x: 0, transition: { delay: 0.1, duration: 0.25 } },
-            viewport: { once: false }
-
+            initial: { opacity: 0.33, x:-50},
+            whileInView: { opacity: 1, x:0, transition: { delay: 0.15, duration: 0.3 } },
+            viewport: { once: false, amount: 0.3 }
         }
-    }
+    
+    
+    // else {
+    //     ;
+    //     LeftsectionVariants = {
+    //         initial: { opacity: 0, x: 75 },
+    //         whileInView: { opacity: 1, x: 0, transition: { delay: 0.1, duration: 0.25 } },
+    //         viewport: { once: false }
 
+    //     }
+    // }
+    
     return (
         <>
-            <section class="grid justify-center text-center lg:grid-cols-2 h-screen">
+              
+        
+            <section class="grid text-center">
                 <motion.div
                     initial={RightsectionVariants.initial}
                     whileInView={RightsectionVariants.whileInView}
                     viewport={RightsectionVariants.viewport}
-                    className='w-full h-screen content-center -mt-40'>
-                    <h1 className='font-medium text-xl lg:text-7xl 2xl:text-9xl text-white mb-10'>Authentifi AI</h1>
-                    <div className='inline-flex mb-30'>
-                        <img src="logo.png" alt="Company Logo" className='h-auto w-15 lg:w-40 mt-4 lg:-mt-8 mr-3 lg:-mr-8' />
-                        <p class='text-lg lg:text-3xl text-gray-300 align-baseline text-left'>AI-powered authorship <br></br>verification</p>
+                    className='content-center pt-30'>
+                    <h1 className='text-5xl text-white font-light'>Authentifi AI</h1>
+                    <div className='inline-flex mt-2'>
+                        <img src="logo.png" alt="Company Logo" className='h-auto w-16' />
+                        <p class='text-xl text-gray-200 text-left'>AI-powered authorship <br></br>verification</p>
                     </div>
-                    <p class="text-4xl lg:text-3xl text-gray-100">Stay in control of your work while using AI</p>
-
-                    <button className='bg-blue-400 rounded-xl text-4xl lg:text-2xl text-white font-bold pt-2 pb-2 pr-4 pl-4 shadow-xl border-1 border-gray-600 hover:bg-green-700 hover:cursor-pointer mt-5 mb-5 '>Register</button>
-
-                    <p class="text-5xl lg:text-3xl text-yellow-300">Be among the first to experience it!</p>
+                    <p class="text-lg font-bold text-gray-100 mt-10 ">Stay in control of your work while using AI</p>
+                    <button className='bg-blue-400 rounded-xl text-xl text-white font-bold pt-2 pb-2 pr-4 pl-4 shadow-xl border-1 border-gray-600 hover:bg-green-700 hover:cursor-pointer mt-5'>Register</button>
+                    <p class="text-xl text-yellow-300 mt-5">Be among the first to experience it!</p>
                 </motion.div>
 
                 <motion.div
                     initial={LeftsectionVariants.initial}
                     whileInView={LeftsectionVariants.whileInView}
                     viewport={LeftsectionVariants.viewport}
-                    class="h-screen text-left p-10 lg:p-5 lg:content-center">
-                    <h2 className='text-gray-200 text-4xl lg:text-3xl mb-20'>Meet our student lucy...</h2>
-                    <video controls className='w-auto h-auto rounded-xl bg-white'>
+                    class="text-left p-5 h-screen content-center">
+                    <h2 className='text-gray-200 text-2xl'>Meet our student lucy...</h2>
+                    <video controls className='rounded-xl bg-white h-auto w-max'>
                         <source src="HeroLandingPageVideo.mp4" type="video/mp4" />
                     </video>
                 </motion.div>
