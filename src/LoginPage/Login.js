6e892a5ab0867015a7e5b9447598f384a183fdd4
AuthentifiAI-Udi <udi@authentifi.ai>
2025-03-14 23:17:48 -0400

    Fixed the design of the login page for mobile support

--- src/LoginPage/Login.js
+++ src/LoginPage/Login.js
@@ -34,46 +34,49 @@ function LoginPage() {
 
   return (
     <>
-      <Navbar></Navbar>
-      <div id="FormContainer" className="bg-white w-fit fixed rounded-xl border-gray-300 border-1 pt-5 pl-5 pr-5 pb-1 text-center">
-        <h3 className="text-gray-900 text-sm">Start your journey</h3>
-        <h2 className="text-black text-xl font-semibold mt-2">Sign in to AuthentifiAI</h2>
-        <form className="flex flex-col justify-center items-center mt-15" onSubmit={handleLogin}>
-          {error && <p style={styles.error}>{error}</p>}
-          {success && <p style={styles.success}>{success}</p>}
-          {/* This is a div that wraps the email input field so i can integrate the email icon into the field itself */}
-          <div className=" relative flex items-center">
-            <input className="LoginInput border-1 rounded-sm text-lg p-2"
-              type="email"
-              placeholder="Enter your email"
-              required
-              pattern="[a-zA-Z0-9.]+@+[a-zA-Z0-9.]+.edu"
-              onInvalid={(e) => e.target.setCustomValidity("Please enter a valid .edu email address.")}
-              onInput={(e) => e.target.setCustomValidity("")}
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }} />
-            <Mail size={20} className="absolute right-3 text-gray-600" />
+        <Navbar className='grow-0 shrink basis-auto'></Navbar>
+        <div className="flex grow shrink-1 basis-auto justify-center items-center">
+          <div id="FormContainer" className="bg-white w-fit h-fit rounded-xl border-gray-300 border-1 text-center pl-5 pr-5 pt-10 pb-2">
+            <h3 className="text-gray-900 text-sm">Start your journey</h3>
+            <h2 className="text-black text-xl font-semibold mt-2">Sign in to AuthentifiAI</h2>
+            <form className="flex flex-col justify-center items-center mt-15" onSubmit={handleLogin}>
+              {error && <p style={styles.error}>{error}</p>}
+              {success && <p style={styles.success}>{success}</p>}
+              {/* This is a div that wraps the email input field so i can integrate the email icon into the field itself */}
+              <div className=" relative flex items-center">
+                <input className="LoginInput border-1 rounded-sm text-md p-2"
+                  type="email"
+                  placeholder="Enter your email"
+                  required
+                  pattern="[a-zA-Z0-9.]+@+[a-zA-Z0-9.]+.edu"
+                  onInvalid={(e) => e.target.setCustomValidity("Please enter a valid .edu email address.")}
+                  onInput={(e) => e.target.setCustomValidity("")}
+                  value={email}
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }} />
+                <Mail size={20} className="absolute right-3 text-gray-600" />
+              </div>
+
+              {/* This is a div that wraps the password input field so i can integrate the eye icon into the field itself */}
+              <div className=" relative flex items-center mt-5">
+                <input className="LoginInput border-1 rounded-sm text-md p-2"
+                  type="password"
+                  placeholder="Enter your password"
+                  value={password}
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }} />
+                <Eye size={20} className="absolute right-3 text-gray-600" />
+              </div>
+
+              <button type="submit" className="mt-10 h-12 w-24 text-lg font-bold bg-white">Login</button>
+              <h5 className="text-sm mt-10 -ml-3">Dont have an account? <a href="/Register">Register</a></h5>
+            </form>
           </div>
 
-          {/* This is a div that wraps the password input field so i can integrate the eye icon into the field itself */}
-          <div className=" relative flex items-center mt-5">
-            <input className="LoginInput border-1 rounded-sm text-lg p-2"
-              type="password"
-              placeholder="Enter your password"
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }} />
-            <Eye size={20} className="absolute right-3 text-gray-600" />
-          </div>
-
-          <button type="submit" className="mt-10 h-12 w-24 text-lg font-bold bg-white">Login</button>
-        </form>
-
-        <h5 className="text-gray-500 text-left text-sm mt-10 -ml-3">Dont have an account? <a href="/Register">Register</a></h5>
-      </div>
+          
+        </div>
     </>
   );
 }
