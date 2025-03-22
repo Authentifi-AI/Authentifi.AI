438a351dac75f99412ffee1fdbac8f9648374eae
AuthentifiAI-Udi <udi@authentifi.ai>
2025-03-22 17:13:04 -0400

    Updated workflow file to new pages test domain git

--- src/HomePage/Hero.js
+++ src/HomePage/Hero.js
@@ -74,7 +74,7 @@ export default function Hero() {
         <>
 
 
-            <section class="grid text-center md:grid-cols-2 md:h-screen md:pb-30 md:mb-30">
+            <section className="grid text-center md:grid-cols-2 md:h-screen md:pb-30 md:mb-30">
                 <motion.div
                     initial={RightsectionVariants.initial}
                     whileInView={RightsectionVariants.whileInView}
@@ -84,12 +84,12 @@ export default function Hero() {
                         <img src="logo.png" alt="Company Logo" className='h-auto w-15 2xl:w-20' />
                         <h1 className='text-5xl text-white font-bold lg:text-6xl lg:ml-2 2xl:text-8xl'>Authentifi AI</h1>
                     </div>
-                    <p class='text-xl lg:text-3xl text-center lg:text-left text-blue-900 font-bold lg:mt-5 2xl:text-4xl 2xl:mt-10'>AI Authership Authentication and Verification</p>
-                    <p class="text-lg font-semibold text-blue-900 mt-10 pl-5 pr-5 italic lg:pl-0 lg:text-3xl lg:mt-15 2xl:mt-30">Use AI with confidence, record your work in real time, and publish with ease.</p>
+                    <p className='text-xl lg:text-3xl text-center lg:text-left text-blue-900 font-bold lg:mt-5 2xl:text-4xl 2xl:mt-10'>AI Authership Authentication and Verification</p>
+                    <p className="text-lg font-semibold text-blue-900 mt-10 pl-5 pr-5 italic lg:pl-0 lg:text-3xl lg:mt-15 2xl:mt-30">Use AI with confidence, record your work in real time, and publish with ease.</p>
 
                     <div className="flex justify-center w-full">
                         <div className="bg-gray-100 w-fit p-4 rounded-2xl border-gray-900 border mt-5 lg:mt-15 2xl:mt-32 justify-items-center lg:p-7">
-                            <p class="text-3xl font-bold text-black md:text-center lg:justify-self-center lg:text-4xl 2xl:text-5xl">Sign up early for free</p>
+                            <p className="text-3xl font-bold text-black md:text-center lg:justify-self-center lg:text-4xl 2xl:text-5xl">Sign up early for free</p>
                             <button onClick={togglePopup} className='bg-blue-400 rounded-xl text-xl text-white font-bold pt-2 pb-2 pr-4 pl-4 shadow-xl border-1 border-gray-600 hover:bg-green-700 hover:cursor-pointer mt-5 lg:text-2xl 2xl:text-3xl'>Register</button>
                         </div>
                     </div>
@@ -99,7 +99,7 @@ export default function Hero() {
                     initial={LeftsectionVariants.initial}
                     whileInView={LeftsectionVariants.whileInView}
                     viewport={LeftsectionVariants.viewport}
-                    class="text-left p-5 h-screen content-center mt-10 mb-10 md:h-auto">
+                    className="text-left p-5 h-screen content-center mt-10 mb-10 md:h-auto">
                     <h1 className='text-white text-3xl font-bold xl:text-4xl xl:mb-3'>See how it works &rarr;</h1>
                     <h2 className='text-blue-900 font-medium  text-2xl xl:text-4xl xl:mb-3'>Meet our student lucy...</h2>
                     <video controls className='rounded-xl bg-white h-auto w-max xl:h-8/12'>
